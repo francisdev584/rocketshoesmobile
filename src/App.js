73c1518cc25cae4d4f65185619dda8c61a05ed4f
Francinildo <francis584@gmail.com>
2019-11-11 12:59:51 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View>Olá React-native</View>
+    </>
+  );
+};
+
+export default App;
