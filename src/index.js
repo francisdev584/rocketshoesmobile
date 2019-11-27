1621ba98125cd31ed943e341811e6be6ce123638
Francinildo <francis584@gmail.com>
2019-11-27 18:20:13 -0300

    Redux installed/configurated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
+import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
+import store from './store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#191920" />
       <Routes />
-    </>
+    </Provider>
   );
 }
 
