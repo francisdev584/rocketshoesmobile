a7143e727dfb65a6bc65a064346eb10a1d95d9d2
Francinildo <francis584@gmail.com>
2019-11-27 18:49:37 -0300

    number of itens in cart - header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 // import {View} from 'react-native';
+import { connect } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Wrapper, Container, Logo, BasketContainer, ItemCount } from './styles';
 
-function Header() {
+function Header({ cartSize }) {
   return (
     <Wrapper>
       <Container>
         <Logo />
         <BasketContainer>
           <Icon name="shopping-basket" color="#FFF" size={24} />
-          <ItemCount>0</ItemCount>
+          <ItemCount>{cartSize}</ItemCount>
         </BasketContainer>
       </Container>
     </Wrapper>
   );
 }
 
-export default Header;
+export default connect(state => ({ cartSize: state.cart.length }))(Header);
