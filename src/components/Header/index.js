cddd0020669e3e05f2cf7e94b73ffed33d028fa2
Francinildo <francis584@gmail.com>
2019-11-27 19:04:46 -0300

    click on the icon cart and go to Cart comp

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,18 +5,21 @@ import { connect } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Wrapper, Container, Logo, BasketContainer, ItemCount } from './styles';
 
-function Header({ cartSize }) {
+function Header({ navigation, cartSize }) {
   return (
     <Wrapper>
       <Container>
         <Logo />
-        <BasketContainer>
+        <BasketContainer onPress={() => navigation.navigate('Cart')}>
           <Icon name="shopping-basket" color="#FFF" size={24} />
-          <ItemCount>{cartSize}</ItemCount>
+          <ItemCount>{cartSize || 0}</ItemCount>
         </BasketContainer>
       </Container>
     </Wrapper>
   );
 }
 
-export default connect(state => ({ cartSize: state.cart.length }))(Header);
+export default connect(
+  state => ({ cartSize: state.cart.length }),
+  null
+)(Header);
